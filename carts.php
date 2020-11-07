5fff137671e65ca0e75c27f766c987cefc9ab55a
Gentlesammy <samodunlade84@gmail.com>
2020-11-07 09:54:37 +0100

    cart.php page set up with total price function

--- carts.php
+++ carts.php
@@ -4,9 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cart Page</title>
+
     <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="./styles/bootstrap.css">
 </head>
 <body>
+    <section class="cartsection">
+        <div class="container py-5">
+            <h1 class="text-center">CART ITEMS</h1>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="productlist">
+                
+                </div>
+            </div>
+
+            <div class="col-md-4 bg-dark pt-2">
+                <h4 class="text-center text-white">Summary</h4>
+                <hr/>
+
+                <div class="form-group px-4">
+                    <label for="" class="text-white pl-2">Coupon Code</label>
+                    <input type="text" name="" id="" class="form-control">
+                    <small class="text-white-50 pl-2">Enter a Valid Code to reduce price</small>
+                    <input type="button" value="Validate"  class="btn btn-primary btn-sm mt-2">
+                </div>
+                <hr/>
+                <div class="benefit px-4 pt-2">
+                    <p class="title">Benefit</p>
+                    <p class="value">0%</p>
+                </div>
+                <hr/>
+                <div class="total px-4 pt-2 ">
+                    <p class="title">Total</p>
+                    <p class="total" id="totalcost"></p>
+                </div>
+                <hr/>
+            </div>
+        </div>
+
+        
+
+        <div class="couponsection">
+        </div>
+
+        <div class="summary">
+        </div>
+        </div>
+    </section>
     
 
     <script src="./scripts/script.js"></script>
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,40 @@
+//get all products in json file to display on page
+const getAllProducts = async () => {
+  const response = await fetch("./scripts/products.json");
+  const products = await response.json();
+  return products;
+};
 
+//display products on cart
+const displayCartOnPage = async () => {
+  const products = await getAllProducts();
+  const productlist = document.querySelector(".productlist");
+  const proMap = products.products.map((pro) => {
+    return `
+            <div class='single'>
+                <h4 class='pro-title'>${pro.name}</h4>
+                <div class='img-container'>
+                  <img src=./images/${pro.coverImage} class='img-fluid'>
+                </div>
+                <h6 class='pro-price'>$${pro.price}</h6>
+            </div>
+    `;
+  });
+
+  productlist.innerHTML = `
+    ${proMap.join("")}
+  `;
+  cartTotalCalc();
+};
+displayCartOnPage();
+
+// calculate cart total and show in the right column
+const cartTotalCalc = async (discount = 0) => {
+  const products = await getAllProducts();
+  var totalPrice = 0;
+  for (let i = 0; i < products.products.length; i++) {
+    totalPrice = totalPrice + products.products[i].price;
+  }
+  const priceCol = document.querySelector("#totalcost");
+  priceCol.innerHTML = "$" + totalPrice;
+};
