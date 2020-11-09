7658f73768402ab6ddc7c7ef14b62219496c5eb9
Gentlesammy <samodunlade84@gmail.com>
2020-11-09 14:34:30 +0100

    coupon cart Items count  requirement done

--- scripts/script.js
+++ scripts/script.js
@@ -64,15 +64,50 @@ couponvalidator.addEventListener("click", () => {
       headers: { "Content-type": "application/json; charset=UTF-8" },
     })
       .then((response) => response.json())
-      .then((json) => {
+      .then(async (json) => {
         if (json.code == 401) {
           //coupon code does not exist or coupon code has expired or coupon code has been deactivated
           alert(json.message);
         } else {
           //coupon code has not expired and has not be invalidated by admin
-          console.log(json);
+          const priceCheckOkay = cartTotalPriceValid(
+            json.coupon_details.cou_priceconstraint
+          );
+          const cartCheckOkay = await cartTotalItemValid(
+            json.coupon_details.cou_qtyconstraint
+          );
+
+          //check if both conditions are met
+          if (priceCheckOkay && cartCheckOkay) {
+            //both are okay, deduct from total
+            console.log({
+              type: json.coupon_details.cou_type,
+              value: json.coupon_details.cou_value,
+              cartCheckOkay,
+            });
+          } else {
+            //return error message, condition for coupon not met
+            alert("Your Cart does not qualify for this coupon");
+          }
         }
       })
       .catch((err) => console.log(err.message));
   }
 });
+
+//coupontotal price constraint checker formula
+const cartTotalPriceValid = () => {
+  return false;
+};
+
+//coupon cart quantity check
+
+const cartTotalItemValid = async (cou_qtyconstraint) => {
+  const products = await getAllProducts();
+  const cTotalItems = products.products.length;
+  if (cTotalItems >= cou_qtyconstraint) {
+    return true;
+  } else {
+    return false;
+  }
+};
